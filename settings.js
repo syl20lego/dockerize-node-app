27e65997232438f08b03b891dbc1e6cf2387d700
Sylvain Legault <sylvain.legault@pj.ca>
2018-04-15 08:05:41 -0400

    default port

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const path = require('path');
 const settings = {
     PROJECT_DIR : __dirname,
     MODULES_DIR :  path.join(__dirname, '/node_modules/'),
-    DEFAULT_PORT: 3000
+    DEFAULT_PORT: 8080
 };
 
 module.exports = settings;
